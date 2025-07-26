96b1c23af97c60ea1bda71f0d9dcd647998f179d
Eduardo <eduardo.araujoihs@gmail.com>
2025-07-26 20:07:17 -0300

    styles

--- src/components/GiftItem.tsx
+++ src/components/GiftItem.tsx
@@ -10,42 +10,40 @@ const GiftItem: React.FC<GiftItemProps> = ({ gift, isUnlocked }) => {
   return (
     <div
       className="relative rounded-lg overflow-hidden shadow-lg w-full aspect-square"
-        style={{
-          backgroundImage: gift.image_url
-            ? `url(${
-                gift.image_url.startsWith("http")
-                  ? gift.image_url
-                  : `/${gift.image_url}`
-              })`
-            : undefined,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundColor: !gift.image_url ? "#f5f5f5" : undefined,
-        }}
+      style={{
+        backgroundImage: gift.image_url
+          ? `url(${
+              gift.image_url.startsWith("http")
+                ? gift.image_url
+                : `/${gift.image_url}`
+            })`
+          : undefined,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: !gift.image_url ? "#f5f5f5" : undefined,
+      }}
     >
-      <div className="absolute inset-0 bg-gradient-to-t from-gray-600/30 to-transparent flex items-end p-6 pointer-events-none">
-          <div>
-            <p className="text-2xl text-shadow-md text-amber-50">
-              {gift.name}
-            </p>
-            <p className="text-shadow-lg text-slate-200">
-              R$ {Math.ceil(gift.value).toLocaleString("pt-BR")}
-            </p>
-          </div>
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-600/30 to-transparent flex items-end p-2 sm:p-6 pointer-events-none">
+        <div>
+          <p className="text-shadow-md text-amber-50 text-[13px] xs:text-base md:text-xl ">
+            {gift.name}
+          </p>
+          <p className="text-shadow-lg text-slate-200 text-xs sm:text-base ">
+            R$ {Math.ceil(gift.value).toLocaleString("pt-BR")}
+          </p>
         </div>
+      </div>
       {isUnlocked && (
-        <div className="absolute bottom-4 left-4">
-          <span
-            className="text-xs font-semibold px-4 py-2 rounded-full shadow-md"
-            style={{
-              backgroundColor: "var(--accent-blue)",
-              color: "white",
-              fontFamily: "var(--font-body)",
-            }}
+        <div className="absolute inset-0 bg-gradient-to-b from-amber-200/80 to-transparent to-40% flex items-start">
+          <div
+            style={{ textShadow: "0 0 10px #5b5b5b" }}
+            className="xs:mt-2 xs:ml-2 xs:px-4 xs:py-2 p-2  text-white bg-transparent flex items-center"
           >
-            Recebido com gratidão 🙏
-          </span>
+            <span className="text-blue-400 font-bold text-[13px] xs:text-2xl  mr-2 xs:mr-4">✓</span>
+            <span className="not-italic xs:italic  tracking-widest text-[10px] xs:text-sm mr-2 xs:mr-4">Recebido</span>
+
+          </div>
         </div>
       )}
     </div>
--- src/components/GiftList.tsx
+++ src/components/GiftList.tsx
@@ -11,13 +11,15 @@ const GiftList: React.FC<GiftListProps> = ({ gifts, totalDonated }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 sm:gap-3 gap-2">
       {gifts
-        .map((gift) => (
-          <GiftItem
-            key={gift.sort_order}
-            gift={gift}
-            isUnlocked={totalDonated >= gift.value}
-          />
-        ))}
+      .slice()
+      .sort((a, b) => a.sort_order - b.sort_order)
+      .map((gift) => (
+        <GiftItem
+        key={gift.sort_order}
+        gift={gift}
+        isUnlocked={gift.pct_acquired == 1}
+        />
+      ))}
     </div>
   );
 };
