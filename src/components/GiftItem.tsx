af3e35241ec792e7a53e057b0aa12c853d23e73f
Eduardo <eduardo.araujoihs@gmail.com>
2025-09-06 18:57:09 -0300

    showing pct conquistado items

--- src/components/GiftItem.tsx
+++ src/components/GiftItem.tsx
@@ -34,15 +34,31 @@ const GiftItem: React.FC<GiftItemProps> = ({ gift, isUnlocked }) => {
           </p>
         </div>
       </div>
+
+      {(gift.pct_acquired > 0 && gift.pct_acquired < 1)  && (
+        <div className="absolute inset-0 bg-gradient-to-b from-amber-300/40 to-transparent to-40% flex items-start">
+          <div
+            style={{ textShadow: "0 0 10px #5b5b5b" }}
+            className="xs:mt-2 xs:ml-2 xs:px-4 xs:py-2 p-2  text-white bg-transparent flex items-center w-full"
+          >
+            <div className="text-shadow-md text-amber-50 text-[13px] xs:text-base md:text-xl">
+              <span className="font-extrabold tracking-wider">{(gift.pct_acquired * 100).toFixed(0)}%</span> Conquistado
+            </div>
+          </div>
+        </div>
+      )}
       {isUnlocked && (
         <div className="absolute inset-0 bg-gradient-to-b from-amber-200/80 to-transparent to-40% flex items-start">
           <div
             style={{ textShadow: "0 0 10px #5b5b5b" }}
             className="xs:mt-2 xs:ml-2 xs:px-4 xs:py-2 p-2  text-white bg-transparent flex items-center"
           >
-            <span className="text-blue-400 font-bold text-[13px] xs:text-2xl  mr-2 xs:mr-4">✓</span>
-            <span className="not-italic xs:italic  tracking-widest text-[10px] xs:text-sm mr-2 xs:mr-4">Recebido</span>
-
+            <span className="text-blue-400 font-bold text-[13px] xs:text-2xl  mr-2 xs:mr-4">
+              ✓
+            </span>
+            <span className="not-italic xs:italic  tracking-widest text-[10px] xs:text-sm mr-2 xs:mr-4">
+              Recebido
+            </span>
           </div>
         </div>
       )}
